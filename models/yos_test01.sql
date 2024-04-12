7f8ba2d2dc98dc2efaf0b745cb0dfc983987c0b5
Junko Yoshizawa <junko.yoshizawa@om-digitalsolutions.com>
2024-04-12 00:12:36 +0000

    使わないsqlを全部削除した 25個くらい

--- models/yos_test01.sql
+++
@@ -1,5 +0,0 @@
-select *
-from
-  IT_TEST_DB.TEST_SCHEMA.PSI_MONTH_TOP10K
-
-limit 20
--- models/yos_wk_04.sql
+++
@@ -1,7 +0,0 @@
-
---  ITEM_CD NULLないよ。。。。 
-select * 
-from 
-    it_test_db.test_schema.psi_month_2309_10_11
-where 
-    ITEM_CD is not null
