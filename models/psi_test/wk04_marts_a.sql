9366cf0eced3808295d959c8f00c8f51e8597bb5
Junko Yoshizawa <junko.yoshizawa@om-digitalsolutions.com>
2024-04-30 05:18:15 +0000

    4/30 SOH_SELL_TEST_view 追加　日付型

--- models/psi_test/wk04_marts_a.sql
+++ models/psi_test/wk04_marts_a.sql
@@ -1,9 +1,11 @@
 -- marts
--- wk04_20240430
+-- wk04_marts_a   wk04_20240430
 
 select 
+    PLANNING_DATA,
 	SCENARIO,
 	ITEM_CD,
+    coalesce(t2.MATERIAL_NAME, t1.ITEM_CD) as ITEM_NAME,
 	YYYYMM,
 	BC_CD,
 	LP_ETD_IN_QUANT,
@@ -12,9 +14,16 @@ select
 	LS_QUANT,
 	I_QUANT
 from 
-    {{ref('wk03_staging')}}
+    {{ref('wk03_staging')}} as t1
+left join
+    IT_TEST_DB.TEST_SCHEMA.SOH_MASTER_NATERUAL as t2  -- アイテム名 ※別の確認で使ったテーブルなので注意！
+on 
+    t1.ITEM_CD = trim(t2.MATERIAL_CODE)
 
 
+-- where ITEM_CD = 'V110030BG000'
+-- and YYYYMM between 202304 and  202401
+-- and SCENARIO in ( '2023/09')
 
 -- {% set SCENARIO_start_column = '2023/09' %}  -- 計画年月 Actual Month
 -- {% set SCENARIO_end_column = '2023/11' %}    -- Base PSI
